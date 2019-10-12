953c1ac84c919caf831b5b5afe1a1ad227e77590
hamstar0 <webutek@gmail.com>
2019-10-11 21:50:35 -0700

    v1.0.2 - Added ranges to some config settings

--- MyConfig.cs
+++ MyConfig.cs
@@ -19,32 +19,48 @@ namespace MountedMagicMirrors {
 		[Header( "Mounted mirror settings" )]
 		[DefaultValue( true )]
 		public bool EnableMountedMagicMirrorRecipe { get; set; } = true;
+
 		[DefaultValue( false )]
 		[ReloadRequired]
 		public bool EnableMountedMagicMirrorEasyModeRecipe { get; set; } = false;
 
+
 		[DefaultValue( true )]
 		public bool IsMountedMagicMirrorBreakable { get; set; } = true;
+
 
 		[DefaultValue( true )]
 		public bool MountedMagicMirrorDropsItem { get; set; } = true;
 
+		////
 
 		[Header( "World generation settings" )]
 		[DefaultValue( true )]
 		public bool GenerateMountedMirrorsForNewWorlds { get; set; } = true;
 
+
+		[Range( 0, 10000 )]
 		[DefaultValue( 64 )]
 		public int MinimumMirrorTileSpacing { get; set; } = 64;
 
+
+		[Range( 0, 10000 )]
 		[DefaultValue( 6 )]
 		public int TinyWorldMirrors { get; set; } = 6;    // SmallWorldPortals / 2
+
+		[Range( 0, 10000 )]
 		[DefaultValue( 10 )]
 		public int SmallWorldMirrors { get; set; } = 10;  // 4200 x 1200 = 5040000
+
+		[Range( 0, 10000 )]
 		[DefaultValue( 16 )]
 		public int MediumWorldMirrors { get; set; } = 16; // 6400 x 1800 = 11520000
+
+		[Range( 0, 10000 )]
 		[DefaultValue( 24 )]
 		public int LargeWorldMirrors { get; set; } = 24;  // 8400 x 2400 = 20160000
+
+		[Range( 0, 10000 )]
 		[DefaultValue( 30 )]
 		public int HugeWorldMirrors { get; set; } = 30;
 	}

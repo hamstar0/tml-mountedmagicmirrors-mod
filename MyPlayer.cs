5ca323fb5bb34c18497a5cf82e9fbdea005f08a3
hamstar0 <webutek@gmail.com>
2020-02-02 07:09:14 -0800

    v1.1.1 - Restructured player Update code files

--- MyPlayer.cs
+++ MyPlayer.cs
@@ -5,7 +5,6 @@ using Terraria.ModLoader.IO;
 using Terraria;
 using HamstarHelpers.Helpers.Debug;
 using HamstarHelpers.Helpers.DotNET.Extensions;
-using HamstarHelpers.Services.Timers;
 using HamstarHelpers.Helpers.World;
 
 
@@ -170,67 +169,6 @@ namespace MountedMagicMirrors {
 
 		public override void PostSavePlayer() {
 			this._CurrentWorldDiscoveredMirrorTiles = null;
-		}
-
-
-		////////////////
-
-		public override void PreUpdate() {
-			if( this.player.whoAmI == Main.myPlayer ) {
-				if( MMMConfig.Instance.DebugModePosition ) {
-					DebugHelpers.Print( "WhereAmI", (this.player.Center/16).ToShortString()+" ("+this.player.Center.ToString()+")" );
-				}
-
-				if( this.IsMapMirrorPicking ) {
-					this.UpdateMapMirrorPicking();
-				}
-			}
-		}
-
-		////
-
-		private void UpdateMapMirrorPicking() {
-			if( !Main.mapFullscreen ) {
-				this.EndMapMirrorPicking();
-				return;
-			}
-
-			bool isClick =	(Main.mouseRight && Main.mouseRightRelease) ||
-							(Main.mouseLeft && Main.mouseLeftRelease);
-
-			if( isClick ) {
-				if( !this.ClickSafetyLock ) {
-					bool isPickingNow = Timers.GetTimerTickDuration( "MMMIsMapMirrorPickingNow" ) > 0;
-
-					if( isPickingNow && this.TargetMirror.HasValue ) {
-						(int TileX, int TileY) target = this.TargetMirror.Value;
-
-						if( this.CurrentWorldDiscoveredMirrorTiles.Contains2D(target.TileX, target.TileY) ) {
-							if( this.TeleportToMirror( target.TileX, target.TileY ) ) {
-								this.EndMapMirrorPicking();
-							}
-						}
-					}
-				}
-			} else {
-				//if( (!Main.mouseLeft && !Main.mouseLeftRelease) && (!Main.mouseRight && !Main.mouseRightRelease) ) {
-				this.ClickSafetyLock = false;
-				//}
-			}
-		}
-
-
-		////////////////
-
-		public void BeginMapMirrorPicking() {
-			this.IsMapMirrorPicking = true;
-			this.ClickSafetyLock = true;
-		}
-
-		public void EndMapMirrorPicking() {
-			this.IsMapMirrorPicking = false;
-			this.ClickSafetyLock = false;
-			Main.mapFullscreen = false;
 		}
 	}
 }
---
+++ MyPlayer_Update.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Terraria.ModLoader;
+using Terraria;
+using HamstarHelpers.Helpers.Debug;
+using HamstarHelpers.Helpers.DotNET.Extensions;
+using HamstarHelpers.Services.Timers;
+
+
+namespace MountedMagicMirrors {
+	partial class MMMPlayer : ModPlayer {
+		public override void PreUpdate() {
+			if( this.player.whoAmI == Main.myPlayer ) {
+				if( MMMConfig.Instance.DebugModePosition ) {
+					DebugHelpers.Print( "WhereAmI", (this.player.Center/16).ToShortString()+" ("+this.player.Center.ToString()+")" );
+				}
+
+				if( this.IsMapMirrorPicking ) {
+					this.UpdateMapMirrorPicking();
+				}
+			}
+		}
+
+		////
+
+		private void UpdateMapMirrorPicking() {
+			if( !Main.mapFullscreen ) {
+				this.EndMapMirrorPicking();
+				return;
+			}
+
+			bool isClick =	(Main.mouseRight && Main.mouseRightRelease) ||
+							(Main.mouseLeft && Main.mouseLeftRelease);
+
+			if( isClick ) {
+				if( !this.ClickSafetyLock ) {
+					bool isPickingNow = Timers.GetTimerTickDuration( "MMMIsMapMirrorPickingNow" ) > 0;
+
+					if( isPickingNow && this.TargetMirror.HasValue ) {
+						(int TileX, int TileY) target = this.TargetMirror.Value;
+
+						if( this.CurrentWorldDiscoveredMirrorTiles.Contains2D(target.TileX, target.TileY) ) {
+							if( this.TeleportToMirror( target.TileX, target.TileY ) ) {
+								this.EndMapMirrorPicking();
+							}
+						}
+					}
+				}
+			} else {
+				//if( (!Main.mouseLeft && !Main.mouseLeftRelease) && (!Main.mouseRight && !Main.mouseRightRelease) ) {
+				this.ClickSafetyLock = false;
+				//}
+			}
+		}
+
+
+		////////////////
+
+		public void BeginMapMirrorPicking() {
+			this.IsMapMirrorPicking = true;
+			this.ClickSafetyLock = true;
+		}
+
+		public void EndMapMirrorPicking() {
+			this.IsMapMirrorPicking = false;
+			this.ClickSafetyLock = false;
+			Main.mapFullscreen = false;
+		}
+	}
+}

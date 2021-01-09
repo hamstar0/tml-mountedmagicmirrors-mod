958ec99c8bfd77b2ad1d29e58eeeb654432f01ac
hamstar0 <webutek@gmail.com>
2021-01-09 12:37:50 -0800

    v1.1.7 - Added MountedMagicMirrorsAPI.GetDiscoveredMirrors(...)

--- MountedMagicMirrors/API.cs
+++ MountedMagicMirrors/API.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Terraria;
 
 
@@ -6,6 +8,16 @@ namespace MountedMagicMirrors {
 	public class MountedMagicMirrorsAPI {
 		public static void OnMirrorCreate( Action<int, int, Item> action ) {
 			MountedMagicMirrorsMod.Instance.OnMirrorCreate.Add( action );
+		}
+
+
+		public static IList<(int tileX, int tileY)> GetDiscoveredMirrors( Player player ) {
+			var myplayer = player.GetModPlayer<MMMPlayer>();
+
+			IEnumerable<(int, int)> tiles = myplayer.CurrentWorldDiscoveredMirrorTiles?.SelectMany(
+				kv => kv.Value.Select( y => (kv.Key, y) )
+			);
+			return tiles.ToList();
 		}
 	}
 }
